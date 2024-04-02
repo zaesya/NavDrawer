2b99b3d6863f2e4568538b5647456039d9543007
KasiarAffan <kaisaraffan1@gmail.com>
2024-04-02 10:32:21 +0700

    init

--- app/src/main/java/com/example/navdrawerexample/MainActivity.java
+++ app/src/main/java/com/example/navdrawerexample/MainActivity.java
@@ -19,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
     private ActionBarDrawerToggle toggle;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)  {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
